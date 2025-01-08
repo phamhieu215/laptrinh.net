1eb9893118850d092ad5c4fbfa1b8ec709d13929
phamhieu215 <pvhncnh@gmail.com>
2025-01-08 16:38:32 +0700

    Add project files.

---
+++ bai1/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+int n;
+Console.Write(" n: ");
+n=int.Parse(Console.ReadLine());
+// Kiểm tra n là số chẵn hay số lẻ
+if (n % 2 == 0)
+{
+    Console.WriteLine($"{n} la so chan.");
+}
+else
+{
+    Console.WriteLine($"{n} la so le.");
+}
+
+// Kiểm tra n là số âm hay không âm
+if (n >= 0)
+{
+    Console.WriteLine($"{n} la so khong am.");
+}
+else
+{
+    Console.WriteLine($" {n} la so am.");
+}
+        
